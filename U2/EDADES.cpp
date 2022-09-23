d5fdf111f1249ad5ec863d30062e16c1050ca287
UP210289 <up210289@alumnos.upa.edu.mx>
2022-09-23 12:45:34 -0500

    Inicio de unidad dos y trabajo con if

---
+++ U2/EDADES.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int edad;
+    cout << "Ingrese la edad a evaluar: " << endl;
+    cin >> edad;
+    if (edad >= 1 && edad <= 150){
+        if (edad > 0 && edad < 31){
+            printf("Primera Edad");
+        }else if (edad > 30 && edad < 61 ){
+            printf("Segunda Edad");
+        }else if (edad > 60 && edad < 91){
+            printf("Tercera Edad");
+        }else if (edad > 90 && edad < 151){
+            printf("Horas extra");
+        }
+        
+        
+    }
+    else{
+        printf("La edad es invalida");
+    }
+    
+
+    return 0;
+}
