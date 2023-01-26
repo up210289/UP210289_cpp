965fa7f1a059d1c5afd883bdc3f281148653dc10
up210289 <up210289@alumnos.upa.edu.mx>
2023-01-26 16:11:06 -0600

    inicio en un nuevo mundo

---
+++ 4th cuatri/NumAl/NumAl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int usuario;
+            Random x = new Random();
+            int aleatorio = x.Next(1, 10);
+            do
+            {
+                Console.WriteLine("Adivina el numero aleatorio entre 1 y 10 ");
+                usuario = int.Parse(Console.ReadLine());
+                if (aleatorio != usuario)
+                {   
+                    if (usuario > aleatorio)
+                    {
+                        Console.WriteLine("El numero aleatorio es menor");  
+                    }else if(usuario < aleatorio)
+                    {
+                        Console.WriteLine("El numero aleatorio es mayor"); 
+                    }
+                    Console.WriteLine("Intentalo otra vez");
+                }
+            } while (aleatorio != usuario);
+            Console.WriteLine("Correcto!!");
+            Console.WriteLine("NUMERO ALEATORIO " + aleatorio);
+        }
+    }
+}
+
